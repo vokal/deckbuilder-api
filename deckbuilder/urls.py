92f2ad7883b322b85384564f3c1b5b909dc41ed7
Adam Bain <adam.bain@vokalinteractive.com>
2016-02-04 19:56:54 -0600

    Add browsable api

--- deckbuilder/urls.py
+++ deckbuilder/urls.py
@@ -16,5 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 
 urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
     url(r'^', include('api.urls')),
 ]
