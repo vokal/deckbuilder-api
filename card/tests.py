2a2c7211c8252510a4d430229de346f54cd047b0
foresmac <foresmac@gmail.com>
2016-02-05 20:50:51 -0600

    add user, card tests

--- card/tests.py
+++ card/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from user.tests import TestUserMixin
+
+
+class CardTests(TestUserMixin, APITestCase):
+
+    def test_get_card_list(self):
+        url = reverse('card-list')
+        response = self.client.get(url, **self.auth)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data) > 0)
+
+    def test_get_card_detail(self):
+        url = reverse('card-list')
+        response = self.client.get(url, **self.auth)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        pk = response.data['results'][0]['id']
+
+        url = reverse('card-detail', kwargs={'pk': pk})
+        response = self.client.get(url, **self.auth)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('text', response.data)
+        self.assertIn('rarity', response.data)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+import base64
+
+
+class TestUserMixin(object):
+
+    def setUp(self):
+        self.username = 'esquire'
+        self.email = 'bill@billadnted.com'
+        self.password = 'a1b2c3d4'
+        self.first_name = 'Bill'
+        self.last_name = 'Preston'
+
+        user = get_user_model().objects.create_user(
+            username=self.username,
+            email=self.email,
+            password=self.password,
+            first_name=self.first_name,
+            last_name=self.last_name)
+        assert(user)
+        self.user = user
+        self.auth = {'Authorization': 'Basic ' + str(base64.b64encode(b'esquire:a1b2c3d4'))}
+
+
+class TestUserRegistration(APITestCase):
+    def test_register_user(self):
+        data = {
+            'username': 'theodore',
+            'email': 'ted@billandted.com',
+            'password': 'z1y2x3w4',
+            'first_name': 'Ted',
+            'last_name': 'Logan'
+        }
+
+        response = self.client.post(reverse('register'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(response.data['id'])
